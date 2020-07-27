51abae2e1fe9e042d5be031dfb90702ad1f607af
tsungtingdu <tsungtingdu@gmail.com>
2020-07-27 18:41:01 +0800

    create skeleton of actions

--- src/components/UrlForm.js
+++ src/components/UrlForm.js
@@ -1,16 +1,39 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import axios from 'axios'
 
 const UrlForm = () => {
+  // state
+  const [longUrl, setLongUrl] = useState('');
+
+  // handlers
+  const handleChange = e => {
+    setLongUrl(e.target.value)
+  }
+
+  const submitData = e => {
+    e.preventDefault()
+  }
+
+  const clearFormData = e => {
+    e.preventDefault()
+    setLongUrl('')
+  }
+
   return (
     <div className="urlForm d-flex justify-content-center align-items-center">
-      <div className="urlForm__container d-flex flex-column">
-        <input type="text" placeholder="Enter your url here" className="urlForm__container__input" />
+      <form className="urlForm__container d-flex flex-column" onSubmit={submitData}>
+
+        <input
+          type="text"
+          placeholder="Enter your url here"
+          className="urlForm__container__input"
+          value={longUrl}
+          onChange={handleChange} />
         <div className="urlForm__container__buttons d-flex">
-          <button className="createButton">Create short url</button>
-          <button className="clearButton">clear</button>
+          <button type="submit" className="createButton" >Create short url</button>
+          <button type="button" className="clearButton" onClick={clearFormData}>clear</button>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
